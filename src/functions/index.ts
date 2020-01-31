fca7555a3380c0c6ddf83173baed8b71abe9c815
hirose504 <hirose504@yahoo.co.jp>
2020-01-31 15:54:05 +0900

    fix firebase deploy error https://github.com/zeit/next.js/issues/8893

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,8 +1,12 @@
 import * as functions from 'firebase-functions'
 import next from 'next'
+import * as path from 'path'
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev, conf: { distDir: 'next' } })
+const app = next({
+  dev,
+  conf: { distDir: `${path.relative(process.cwd(), __dirname)}/next` }
+})
 const handle = app.getRequestHandler()
 
 // eslint-disable-next-line import/prefer-default-export
